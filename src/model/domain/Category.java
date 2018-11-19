91b87dd5d578910be584cf9de9cd398a33a80656
stevenzegers <steven.zegers@student.ucll.be>
2018-11-19 17:49:48 +0100

    kleine aanpassingen

--- src/model/domain/Category.java
+++ src/model/domain/Category.java
@@ -1,4 +1,36 @@
 package model.domain;
 
 public class Category {
+    private String title;
+    private String description;
+    private Category category;
+    public Category(String title, String description, Category category) {
+        setTitle(title);
+        setDescription(description);
+        setCategory(category);
+    }
+
+    private void setCategory(Category category) {
+        this.category = category;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
 }
